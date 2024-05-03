01fd6b720537c89cbc0453bd422ddc639a1867a6
Atin Mathur <mathuratin007@gmail.com>
2024-05-03 21:28:47 +0530

    Added App Bootstrap component

---
+++ src/components/app-bootstrap/app-bootstrap.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, useCallback } from "react";
+import { View } from "react-native";
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_700Bold,
+} from "@expo-google-fonts/lato";
+import * as SplashScreen from "expo-splash-screen";
+import styles from "./app-bootstrap.styles";
+
+type AppBootstrapProps = {
+  children: ReactNode;
+};
+
+export default function AppBootstrap({ children }: AppBootstrapProps) {
+  const [fontsLoaded, fontError] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+  });
+
+  const onLayoutView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutView}>
+      {children}
+    </View>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,18 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
-import {
-  useFonts,
-  Lato_400Regular,
-  Lato_700Bold,
-} from "@expo-google-fonts/lato";
-import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { Text } from "@components";
+import { AppBootstrap, Text } from "@components";
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    Lato_400Regular,
-    Lato_700Bold,
-  });
-
-  const onLayoutView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
   return (
-    <View style={styles.container} onLayout={onLayoutView}>
-      <StatusBar style="auto" />
-      <Text weight="400" style={{ color: "red", fontSize: 24 }}>
-        Index
-      </Text>
-    </View>
+    <AppBootstrap>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text weight="400" style={{ color: "red", fontSize: 24 }}>
+          Index
+        </Text>
+      </View>
+    </AppBootstrap>
   );
 }
 
