70d2bada91b287e27032f7936a02964b5db0768d
Atin Mathur <mathuratin007@gmail.com>
2024-05-03 20:01:44 +0530

    Added custom google fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
+import React, { useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { Game, Home } from "@screens";
+import { useFonts, Lato_400Regular } from "@expo-google-fonts/lato";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Lato_400Regular,
+  });
+
+  const onLayoutView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Home />
-      <Game />
+    <View style={styles.container} onLayout={onLayoutView}>
       <StatusBar style="auto" />
+      <Text style={{ fontFamily: "Lato_400Regular", fontSize: 24 }}>Index</Text>
     </View>
   );
 }
