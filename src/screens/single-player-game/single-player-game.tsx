95204cf0ccb5bf1dc45ac3263bd05cdb5d83a44a
Atin Mathur <mathuratin007@gmail.com>
2024-05-05 10:21:12 +0530

    Added Board component and utility functions

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,10 +1,31 @@
 import React, { ReactElement } from "react";
 import { GradientBackground, Text } from "@components";
+import { Board } from "@components";
+import {
+  BoardState,
+  getAvailableMoves,
+  isEmpty,
+  isFull,
+  printFormattedBoard,
+} from "@utils";
 
 export default function SinglePlayerGame(): ReactElement {
+  const b: BoardState = ["x", "o", "x", "o", "x", "x", "o", "x", "o"];
+
+  printFormattedBoard(b);
+  console.log(isEmpty(b));
+  console.log(isFull(b));
+  console.log(getAvailableMoves(b));
   return (
     <GradientBackground>
       <Text style={{ color: "#fff" }}>Game</Text>
+      <Board
+        onCellPressed={(index) => {
+          alert(index);
+        }}
+        state={b}
+        size={300}
+      />
     </GradientBackground>
   );
 }
